048443ebfb1ff2911e6a96ba6527db19ea09064e
sahill-1 <sv260397@gmail.com>
2023-05-01 12:41:31 +0530

    seventy four

---
+++ redux/src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h2>Buy Adidas Shoes</h2>
+      <button type="button" class="btn btn-primary mx-2">-</button>
+        Add To Cart
+      <button type="button" class="btn btn-primary mx-2">+</button>
+    </div>
+  )
+}
+
+export default Shop
