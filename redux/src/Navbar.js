0ba714283332dc41193271aaa30898c94ed7f931
sahill-1 <sv260397@gmail.com>
2023-05-02 11:41:08 +0530

    withdraw/deposit money

--- redux/src/Navbar.js
+++ redux/src/Navbar.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
+    
+    const amount = useSelector(state => state.amount)
     return (
         <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">State Bank</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
-                    </button>
+                    </button> */}
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
@@ -19,7 +22,7 @@ const Navbar = () => {
                             </li>
                         </ul>
                         <div>
-                            <button disabled={true} type="button" className="btn btn-primary">Bank Balance: 0</button>
+                            <button disabled={true} type="button" className="btn btn-primary">Bank Balance: {amount}</button>
                         </div>
                     </div>
                 </div>
